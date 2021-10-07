f11804ada56755a554a52233fe677025f4b6a739
Michael Legleux <legleux@gmail.com>
2021-10-07 09:58:08 -0700

    -y

--- test/install.sh
+++ test/install.sh
@@ -2,10 +2,10 @@
 
 if [ "$ID" = "centos" ]; then
   . ./packages/rpm/build_vars
-  yum install ./packages/rpm/$rpm_version_release.x86_64.rpm
+  yum install -y ./packages/rpm/$rpm_version_release.x86_64.rpm
 elif [ "$ID" = "ubuntu" ]; then
   . ./packages/dpkg/build_vars
-  apt install ./packages/dpkg/rippled_"$dpkg_full_version"_amd64.deb
+  apt install -y ./packages/dpkg/rippled_"$dpkg_full_version"_amd64.deb
 else
   echo "Unsupported OS!"
 fi
